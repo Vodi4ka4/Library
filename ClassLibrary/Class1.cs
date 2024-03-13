e25dc8bec54df5f92d4d80d2a9c17cc432f23190
Vodi4ka4 <kolya.hrenov2018@gmail.com>
2024-03-14 01:06:35 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class DateTimeFunctions
+    {
+        public static TimeSpan DifferenceBetweenDates(DateTime date1, DateTime date2)
+        {
+            return date2 - date1;
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            return (year % 4 == 0);
+        }
+
+        public static string TimeOfDay(int hour, int minute)
+        {
+            if(hour > 23|| minute < 0 || minute > 59)
+            {
+                return "Некорректное время";
+            }
+            else if (hour < 5)
+            {
+                return "Ночь";
+            }
+            else if (hour < 12)
+            {
+                return "Утро";
+            }
+            else if (hour < 18)
+            {
+                return "День";
+            }
+            else
+            {
+                return "Вечер";
+            }
+        }
+
+        public static string FormatDateTime(DateTime dateTime)
+        {
+            return dateTime.ToString("dd-MM-yyyy HH:mm:ss");
+        }
+    }
+}
---
+++ Library/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary;
+
+namespace Library
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime1 = dateTimePicker1.Value;
+            DateTime dateTime2 = dateTimePicker2.Value;
+            label1.Text = Convert.ToString(DateTimeFunctions.DifferenceBetweenDates(dateTime1,dateTime2));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = Convert.ToString(DateTimeFunctions.IsLeapYear(Convert.ToInt32(textBox1.Text)));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label6.Text = Convert.ToString(DateTimeFunctions.TimeOfDay(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text)));
+        }
+    }
+}
